c1f7941e69ecd53998f0bfcc14abec62dd0079d1
Sutrisno <sutrisno.stz@bsi.ac.id>
2023-05-21 08:14:21 +0700

    perusahaan acc/banned ok

--- application/views/admin/temp/v_sidebar.php
+++ application/views/admin/temp/v_sidebar.php
@@ -72,16 +72,16 @@
         </li>
         <li class="treeview"> <a href="#"> <i class="fa fa-database mr-2"></i> <span>Perusahaan</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
           <ul class="treeview-menu">
-            <li class="ml-4"><a href="<?php echo base_url('admin/master/admin') ?>"> <i class="fa fa-user-o mr-1"></i>Belum ACC</a></li>
-            <li class="ml-4"><a href="<?php echo base_url('admin/master/perusahaan') ?>"> <i class="fa fa-building mr-1"></i> Perusahaan</a></li>
-            <li class="ml-4"><a href="<?php echo base_url('admin/master/pelamar') ?>"> <i class="fa fa-user mr-1"></i>Banned</a></li>
+            <li class="ml-4"><a href="<?php echo base_url('admin/master/perusahaanadmin') ?>"> <i class="fa fa-window-close mr-1"></i>Belum ACC</a></li>
+            <li class="ml-4"><a href="<?php echo base_url('admin/master/perusahaanadmin/acc') ?>"> <i class="fa fa-check-square-o mr-1"></i> Perusahaan</a></li>
+            <li class="ml-4"><a href="<?php echo base_url('admin/master/perusahaanadmin/ban') ?>"> <i class="fa fa-ban mr-1"></i>Banned</a></li>
 
           </ul>
         </li>
         <li class="treeview"> <a href="#"> <i class="fa fa-database mr-2"></i> <span>Lowongan</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
           <ul class="treeview-menu">
-            <li class="ml-4"><a href="<?php echo base_url('admin/master/lowonganadmin') ?>"> <i class="fa fa-user-o mr-1"></i>Belum ACC</a></li>
-            <li class="ml-4"><a href="<?php echo base_url('admin/master/lowonganadmin/acc') ?>"> <i class="fa fa-building mr-1"></i>Lowongan ACC</a></li>
+            <li class="ml-4"><a href="<?php echo base_url('admin/master/lowonganadmin') ?>"> <i class="fa fa-window-close mr-1"></i>Belum ACC</a></li>
+            <li class="ml-4"><a href="<?php echo base_url('admin/master/lowonganadmin/acc') ?>"> <i class="fa fa-check-square-o mr-1"></i>Lowongan ACC</a></li>
 
 
           </ul>
