26f2a118bd818188be8843c316abd97cf06adc90
Sutrisno <sutrisno.stz@bsi.ac.id>
2023-05-26 07:30:25 +0700

    tambah halaman depan

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,15 +1,15 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Depan extends CI_Controller
+class Home extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         //$this->load->helper('url');
-        if ($this->session->userdata('status') <> 'login') {
-            redirect(base_url('login'));
-        }
+        // if ($this->session->userdata('status') <> 'login') {
+        //     redirect(base_url('login'));
+        // }
         //Codeigniter : Write Less Do More
     }
 
@@ -31,10 +31,10 @@ class Depan extends CI_Controller
     public function index()
     {
         // $data['spp'] = $this->db->query("select * from tbl_spp S, tbl_tahunajaran T, tbl_admin A where S.kd_tahunajaran=T.kd_tahunajaran and S.kd_admin=A.kd_admin")->result();
-        $this->load->view('admin/temp/v_header');
-        $this->load->view('admin/temp/v_atas');
-        $this->load->view('admin/temp/v_sidebar');
-        $this->load->view('admin/temp/v_dasboard');
-        $this->load->view('admin/temp/v_footer');
+        // $this->load->view('admin/temp/v_header');
+        // $this->load->view('admin/temp/v_atas');
+        // $this->load->view('admin/temp/v_sidebar');
+        $this->load->view('depan/v_isi');
+        // $this->load->view('admin/temp/v_footer');
     }
 }
